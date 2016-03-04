f85036bb30d994ad83909ab9e09cc785a5173a43
StringEisen <eisennoah@outlook.com>
2016-03-04 17:46:43 +0800

    complete testing BruteCollinearPoints.java

--- Mergesort & Quicksort/assignment/BruteCollinearPoints.java
+++ Mergesort & Quicksort/assignment/BruteCollinearPoints.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Arrays;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
 
 public class BruteCollinearPoints {
     // contains all linesegments in this arraylist
     private ArrayList<LineSegment> segmentList;
-    private LineSegment[] segments;
     
     private void check(Point[] points) {
         if (points == null) throw new NullPointerException("null array");
@@ -15,7 +17,6 @@ public class BruteCollinearPoints {
                 if (points[j].compareTo(points[i]) == 0) throw new IllegalArgumentException("repeated points");
         }
     }
-    
     
     /**
      * finds all line segments containing 4 points
@@ -28,14 +29,16 @@ public class BruteCollinearPoints {
         check(points);
         int l = points.length;
         segmentList = new ArrayList<LineSegment>();
-        // the points array should be first sorted into ascending order to find endpoints
-        Comparator<Point> order = points[0].slopeOrder();
-        Arrays.sort(points, order);
+        // the points array should be first sorted into ascending order (natrual order) to find endpoints
+        Arrays.sort(points);
+        //Comparator<Point> order = points[0].slopeOrder();
+        //for (Point p : points)
+            //StdOut.println(p);
         // should find all combinations of 4 points
         for (int i = 0; i < l; i++) {
             for (int j = i + 1; j < l; j++) {
                 for (int n = j + 1; n < l; n++) {
-                    //Comparator<Point> order = points[i].slopeOrder();
+                    Comparator<Point> order = points[i].slopeOrder();
                     if (order.compare(points[j], points[n]) != 0) continue;
                     for(int m = n + 1; m < l; m++) {
                         if (order.compare(points[n], points[m]) != 0) continue;
@@ -58,10 +61,39 @@ public class BruteCollinearPoints {
      * @return LineSegment[] line segments that connects at least 4 points
      */
     public LineSegment[] segments() {
-        segments = new LineSegment[numberOfSegments()];
+        LineSegment[] segments = new LineSegment[numberOfSegments()];
         for (int i = 0; i < segments.length; i++) {
             segments[i] = segmentList.get(i);
         }
         return segments;
+    }
+    
+    public static void main(String[] args) {
+        
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+        
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+        
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
     }
 }
