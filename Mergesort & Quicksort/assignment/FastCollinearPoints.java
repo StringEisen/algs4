c904ca3e1a34f1d4109dc05a69cf6bc0e68d001f
StringEisen <eisennoah@outlook.com>
2016-03-08 12:08:11 +0800

    update fast

--- Mergesort & Quicksort/assignment/FastCollinearPoints.java
+++ Mergesort & Quicksort/assignment/FastCollinearPoints.java
@@ -10,38 +10,54 @@ import edu.princeton.cs.algs4.In;
  */
 
 public class FastCollinearPoints {
-    private ArrayList<LineSegment> list;
+    private LineSegment[] segments;
     
     public FastCollinearPoints(Point[] points) {
-        list = new ArrayList<LineSegment>();
+        check(points);
+        ArrayList<LineSegment> list = new ArrayList<LineSegment>();
+        //Comparator<Point> order = points[0].slopeOrder();
+        //Arrays.sort(points, order);
         for (int i = 0; i < points.length; i++) {
             Comparator<Point> order = points[i].slopeOrder();
             Arrays.sort(points, order);
-            // check if any 3 or more adjacent points have equal slope
-            for (int j = 1; j < points.length; j++) {
-                double slope = points[i].slopeTo(points[j]);
-                int n;
-                for (n = 0; j+n < points.length && points[i].slopeTo(points[j+n]) == slope; n++) ;
-                if (n >= 2) {
-                    LineSegment line = new LineSegment(points[i], points[j]);
+            // point i has the index 0 after sorting by itself
+            for (int j = 1; j < points.length; ) {
+                // j is the beginning of the adjacent 3 collinear points
+                int n = j;
+                while (j < points.length-1 && order.compare(points[j], points[j+1]) == 0) { j++; }
+                if (j-n >= 2) {
+                    // this line has error as endpoints are not ones at the begining and end
+                    LineSegment line = new LineSegment(points[0], points[j]);
                     list.add(line);
                 }
+                //StdOut.println("j = " + j);
+                j++;
             }
         }
+        segments = new LineSegment[list.size()];
+        for (int i = 0; i < segments.length; i++) 
+            segments[i] = list.get(i);
     }
+
     
     public int numberOfSegments() {
-        return list.size();
+        return segments.length;
     }
     
     public LineSegment[] segments() {
-        LineSegment[] segments = new LineSegment[numberOfSegments()];
-        for (int i = 0; i < segments.length; i++) 
-            segments[i] = list.get(i);
         return segments; 
     }
     
-        public static void main(String[] args) {
+    private void check(Point[] points) {
+        if (points == null) throw new NullPointerException("null array");
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) throw new NullPointerException("null point");
+            for (int j = i+1; j < points.length; j++)
+                if (points[j].compareTo(points[i]) == 0) throw new IllegalArgumentException("repeated points");
+        }
+    }
+    
+    public static void main(String[] args) {
         
         // read the N points from a file
         In in = new In(args[0]);
@@ -63,7 +79,7 @@ public class FastCollinearPoints {
         StdDraw.show();
         
         // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
             StdOut.println(segment);
             segment.draw();
